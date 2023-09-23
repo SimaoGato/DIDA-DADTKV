d114eefd93e404b2309f856cf44d97f1019481f0
SimaoGato <simao.valedegato@gmail.com>
2023-09-23 18:20:40 +0100

    Implement simple version of Client

---
+++ DADTKV/Client/ClientService.cs
@@ -0,0 +1,53 @@
+﻿using Grpc.Net.Client;
+
+namespace Client;
+
+public class ClientService
+{
+    
+    private DadtkvClientService.DadtkvClientServiceClient _clientStub;
+    
+    public ClientService(string serverHostname, int serverPort) 
+    {
+        var channel = GrpcChannel.ForAddress("http://" + serverHostname + ":" + serverPort.ToString());
+        _clientStub = new DadtkvClientService.DadtkvClientServiceClient(channel);
+    }
+
+    public void TxSubmit(string clientId, List<string> objectsToRead, List<Dictionary<string, int>> objectsToWrite)
+    {
+        var request = new TransactionRequest
+        {
+            ClientId = clientId,
+            ObjectsToRead = { objectsToRead }
+        };
+
+        var dadIntList = new List<DadInt>();
+        
+        foreach (var dadIntObject in objectsToWrite)
+        {
+
+            var dadInt = new DadInt
+            {
+                Key = dadIntObject.Keys.First(),
+                Value = dadIntObject.Values.First()
+            };
+            
+            dadIntList.Add(dadInt);
+            
+        }
+
+        request.ObjectsToWrite.AddRange(dadIntList);
+        
+        _clientStub.TxSubmit(request);
+        
+    }
+
+    public void Status()
+    {
+        var request = new StatusRequest();
+        
+        var reply = _clientStub.Status(request);
+        
+        Console.WriteLine("Status: {0}", reply.Status);
+    }
+}
--- DADTKV/Client/Program.cs
+++ DADTKV/Client/Program.cs
@@ -1,3 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
+using Client;
 
-Console.WriteLine("Goodbye, World!");
+class Program
+{
+    static private void Main()
+    {
+        string serverHostname = "localhost";
+        int serverPort = 5000;
+
+        var clientService = new ClientService(serverHostname, serverPort);
+
+        Console.Write("Enter client ID: ");
+        string clientId = Console.ReadLine();
+
+        bool exit = false;
+
+        while (!exit)
+        {
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1. Submit a transaction");
+            Console.WriteLine("2. Check status");
+            Console.WriteLine("3. Exit");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("Enter objects to read (comma-separated): ");
+                    List<string> objectsToRead = new List<string>(Console.ReadLine().Split(','));
+
+                    Console.WriteLine("Enter objects to write (key:value,key:value): ");
+                    List<Dictionary<string, int>> objectsToWrite = new List<Dictionary<string, int>>();
+                    string[] writeInputs = Console.ReadLine().Split(',');
+
+                    foreach (string input in writeInputs)
+                    {
+                        string[] keyValue = input.Split(':');
+                        if (keyValue.Length == 2)
+                        {
+                            string key = keyValue[0];
+                            int value;
+                            if (int.TryParse(keyValue[1], out value))
+                            {
+                                objectsToWrite.Add(new Dictionary<string, int> { { key, value } });
+                            }
+                        }
+                    }
+
+                    clientService.TxSubmit(clientId, objectsToRead, objectsToWrite);
+                    break;
+
+                case "2":
+                    clientService.Status();
+                    break;
+
+                case "3":
+                    exit = true;
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice. Please enter a valid option.");
+                    break;
+            }
+        }
+
+        Console.WriteLine("Exiting the client application.");
+    }
+}
