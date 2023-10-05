2b5eef9655cac459f83eb79ca0a447ea49f3a17a
SimaoGato <simao.valedegato@gmail.com>
2023-10-05 15:47:18 +0100

    [Fix] Add leaseId to LeaseManagerLogic

--- DADTKV/LeaseManager/LeaseManagerLogic.cs
+++ DADTKV/LeaseManager/LeaseManagerLogic.cs
@@ -3,31 +3,41 @@
         private string[] _args;
         public string lmNick;
         public string lmUrl;
+        public int lmId;
         public int numberOfLm;
+        public List<string> lmServers;
         public int numberOfTm;
+        public List<string> tmServers;
         public int timeSlots;
         public int slotDuration;
         private int slotBehaviorCount;
+        public List<KeyValuePair<string, string>> slotBehavior;
         public DateTime startTime;
 
         public LeaseManagerLogic(string[] args) {
             _args = args;
             lmNick = args[0];
             lmUrl = args[1];
-            numberOfLm = int.Parse(args[2]);
-            numberOfTm = int.Parse(args[3 + numberOfLm * 2]);
+            lmId = int.Parse(args[2]);
+            numberOfLm = int.Parse(args[3]);
+            lmServers = ParseLmServers();
+            numberOfTm = int.Parse(args[4 + numberOfLm * 2]);
+            tmServers = ParseTmServers();
             var argBreaker = Array.IndexOf(args, "-");
             timeSlots = int.Parse(args[argBreaker + 1]);
             slotDuration = int.Parse(args[argBreaker + 2]);
             slotBehaviorCount = int.Parse(args[argBreaker + 3]);
+            slotBehavior = ParseSlotBehavior();
             startTime = DateTime.ParseExact(args[^1], "HH:mm:ss", null);
+            
+            //PrintArgs();
         }
         
         public List<string> ParseTmServers() {
             List<string> tmServers = new List<string>();
             for(int i = 0; i < numberOfTm; i++)
             {
-                tmServers.Add(_args[5 + numberOfLm * 2 + i * 2]);
+                tmServers.Add(_args[6 + numberOfLm * 2 + i * 2]);
             }
             return tmServers;
         }
@@ -36,9 +46,9 @@
             List<string> lmServers = new List<string>();
             for(int i = 0; i < numberOfLm; i++)
             {
-                if (_args[4 + i * 2] != lmUrl)
+                if (_args[5 + i * 2] != lmUrl)
                 {
-                    lmServers.Add(_args[4 + i * 2]);
+                    lmServers.Add(_args[5 + i * 2]);
                 }
             }
             return lmServers;
@@ -60,6 +70,31 @@
                 slotBehavior.Add(keyValuePair);
             }
             return slotBehavior;
+        }
+
+        public void PrintArgs()
+        {
+            Console.WriteLine("lmNick: " + lmNick);
+            Console.WriteLine("lmUrl: " + lmUrl);
+            Console.WriteLine("lmId: " + lmId);
+            Console.WriteLine("LmServers: ");
+            foreach (var lmServer in lmServers)
+            {
+                Console.WriteLine(lmServer);
+            }
+            Console.WriteLine("TmServers: ");
+            foreach (var tmServer in tmServers)
+            {
+                Console.WriteLine(tmServer);
+            }
+            Console.WriteLine("slotBehavior: ");
+            foreach (var slot in slotBehavior)
+            {
+                Console.WriteLine(slot.Key + "-" + slot.Value);
+            }
+            Console.WriteLine("timeSlots: " + timeSlots);
+            Console.WriteLine("slotDuration: " + slotDuration);
+            Console.WriteLine("----------");
         }
     }
 }
