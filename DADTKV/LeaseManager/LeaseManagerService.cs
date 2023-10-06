d60408c7a1eb977943589daf5454d0d844b7c7a3
SimaoGato <simao.valedegato@gmail.com>
2023-10-06 12:07:09 +0100

    Add missing files

---
+++ DADTKV/LeaseManager/LeaseManagerService.cs
@@ -0,0 +1,43 @@
+﻿using Grpc.Net.Client;
+
+namespace LeaseManager;
+
+public class LeaseManagerService
+{
+    private List<LeaseResponseService.LeaseResponseServiceClient> _transactionManagersStubs = 
+        new List<LeaseResponseService.LeaseResponseServiceClient>();
+
+    public LeaseManagerService(List<string> tmAddresses)
+    {
+        foreach (var tmAddress in tmAddresses)
+        {
+            var channel = GrpcChannel.ForAddress(tmAddress);
+            _transactionManagersStubs.Add(new LeaseResponseService.LeaseResponseServiceClient(channel));
+        }
+    }
+    
+    public bool SendLeases(List<List<string>> leases)
+    {
+        var request = new SendLeaseRequest();
+
+        foreach (var l in leases)
+        {
+            var lease = new LeaseOrder();
+            lease.Value.AddRange(l);
+            request.Leases.Add(lease);
+        }
+
+        foreach (var tmStub in _transactionManagersStubs)
+        {
+            var response = tmStub.SendLeases(request);
+            if (!response.Ack)
+            {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+    
+}
---
+++ DADTKV/TransactionManager/LeaseManagerServiceImpl.cs
@@ -0,0 +1,42 @@
+﻿using Grpc.Core;
+
+namespace TransactionManager;
+
+public class LeaseManagerServiceImpl : LeaseResponseService.LeaseResponseServiceBase
+{
+    
+    private int _numberOfLeaseManagers;
+    private TransactionManagerState _transactionManagerState;
+    public bool canExecuteTransactions = false;
+    
+    public LeaseManagerServiceImpl(TransactionManagerState transactionManagerState, int numberOfLeaseManagers)
+    {
+        _transactionManagerState = transactionManagerState;
+        _numberOfLeaseManagers = numberOfLeaseManagers;
+    }
+ 
+    public override Task<SendLeaseResponse> SendLeases(SendLeaseRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(DoSendLeases(request));
+    }
+
+    private SendLeaseResponse DoSendLeases(SendLeaseRequest request)
+    {
+        List<List<string>> leases = new List<List<string>>();
+        
+        foreach (var lease in request.Leases)
+        {
+            leases.Add(lease.Value.ToList());
+        }
+
+        _transactionManagerState.ReceiveLeasesOrder(leases);
+
+        var response = new SendLeaseResponse
+        {
+            Ack = true,
+        };
+
+        return response;
+    }
+    
+}
